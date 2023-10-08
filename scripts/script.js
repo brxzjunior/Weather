dba8e4bc3f648c9a2aae0ded50316b1ce4a82338
Braz Junior <90149109+brxzjunior@users.noreply.github.com>
2023-10-07 20:42:34 -0400

    First big update
    New background, animaton and more!
    *Alert when the input recognize wrong city name
    *BG animations
    *More

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,36 @@
-const key ="d90ff67156bc289cf5e79d6be28f583f"
+const key = "d90ff67156bc289cf5e79d6be28f583f"
 
-function dataOnScreen(data){
-    document.querySelector(".city").innerHTML = "Weather in " + data.name
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °C"
-    document.querySelector(".prev").innerHTML = data.weather[0].description
-    document.querySelector(".cd").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+function dataOnScreen(data) {
+    if (data.name) {
+        document.querySelector(".city").innerHTML = "Weather in " + data.name
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °C"
+        document.querySelector(".prev").innerHTML = data.weather[0].description
+        document.querySelector(".cd").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    } else {
+        alert("Please enter a valid city name!")
+    }
 }
 
-async function citySearch(city){
+async function citySearch(city) {
     const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`).then(response => response.json())
-    
+
     dataOnScreen(data)
 }
 
-function btnsearch(){
+function btnsearch() {
     const city = document.querySelector(".input-city").value
 
     citySearch(city)
 }
 
-//add 'enter' to the search 
-document.addEventListener("DOMContentLoaded", function() {
-    
-    const inputElement = document.querySelector(".input-city");
+//Enter to search
+document.addEventListener("DOMContentLoaded", function () {
+    const inputElement = document.querySelector(".input-city")
 
-    
-    inputElement.addEventListener("keydown", function(event) {
+    inputElement.addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
-           
-            event.preventDefault();
-            
-            btnsearch();
+            event.preventDefault()
+            btnsearch()
         }
     })
 })
-
