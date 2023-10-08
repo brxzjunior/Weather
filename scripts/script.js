5edba2070e7aea6445be87190b6f8fbaa0a6b621
Braz Junior <90149109+brxzjunior@users.noreply.github.com>
2023-10-07 20:12:49 -0400

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ const key ="d90ff67156bc289cf5e79d6be28f583f"
 
 function dataOnScreen(data){
     document.querySelector(".city").innerHTML = "Weather in " + data.name
-    document.querySelector(".temp").innerHTML = Math.floor(data.main.temp) + " °C"
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °C"
     document.querySelector(".prev").innerHTML = data.weather[0].description
     document.querySelector(".cd").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
 }
@@ -19,17 +19,19 @@ function btnsearch(){
     citySearch(city)
 }
 
+//add 'enter' to the search 
 document.addEventListener("DOMContentLoaded", function() {
-    // Seleciona o campo de entrada pelo ID "enter"
+    
     const inputElement = document.querySelector(".input-city");
 
-    // Adiciona um ouvinte de evento para a tecla "Enter"
+    
     inputElement.addEventListener("keydown", function(event) {
         if (event.key === "Enter") {
-            // Evita o comportamento padrão do formulário (não recarrega a página)
+           
             event.preventDefault();
-            // Chama a função btnsearch() quando a tecla "Enter" for pressionada
+            
             btnsearch();
         }
-    });
-});
+    })
+})
+
