63a539d3c4efbf9c8e1dbf616626cf070da88897
wiliami <wiliaup97@gmail.com>
2023-11-13 18:00:31 -0400

    Incluindo uma div contendo a mensagem de erro

--- scripts/script.js
+++ scripts/script.js
@@ -2,20 +2,21 @@ const key = "d90ff67156bc289cf5e79d6be28f583f"
 
 function dataOnScreen(data) {
     if (data.name) {
+        showError.style.display = 'none';
         document.querySelector(".city").innerHTML = "Clima em " + data.name
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C"
         document.querySelector(".prev").innerHTML = data.weather[0].description
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%"
         document.querySelector(".weather-icon").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
     } else {
-        alert("Por favor, insira um nome de cidade válido!")
+        showError.style.display = 'block';
     }
 }
 
 async function citySearch(city) {
     const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json())
 
-    dataOnScreen(data)
+    dataOnScreen(data)  
 }
 
 function btnsearch() {
